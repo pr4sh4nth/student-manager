3dffd6ee0451ba45ac187edf9328a2aa33a257a8
alex zanfir <alexzanfir@gmail.com>
2019-05-16 10:57:12 -0700

    root

---
+++ src/components/Students.vue
@@ -0,0 +1,31 @@
+<template>
+ <v-flex sm8 offset-sm2>
+    <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>Students</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile v-for="(student, index) in students" :to="'editStudent/' + index">
+          <v-list-tile-content>
+            <v-list-tile-title v-text="student.name"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            students: []
+        }
+    },
+  async created() {
+      this.students = (await axios.get('http://localhost:3000/students')).data;
+  }
+};
+</script>
